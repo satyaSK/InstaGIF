2514a3f5e785f09694dec3496877956aac276c38
satyaSK <satyajit.k@somaiya.edu>
2020-03-29 20:18:23 -0700

    init commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 const express = require("express")
 const path = require('path')
+const http = require('http')
+
 
 const port = process.env.PORT || 3000
 
 const PATH = path.join(__dirname,"../public")
 
 app = express()
-
+const server = http.createServer(app)
 app.use(express.static(PATH))
 
-app.listen(3000,()=>{
+server.listen(3000,()=>{
     console.log(`Server live at port: ${port}`)
 })
