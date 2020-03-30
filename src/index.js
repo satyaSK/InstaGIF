c7a4f62b480fc989f7a3b65780d27f6c28862ec8
satyaSK <satyajit.k@somaiya.edu>
2020-03-29 19:12:39 -0700

    init commit

--- src/index.js
+++ src/index.js
@@ -7,7 +7,9 @@ const PATH = path.join(__dirname,"../public")
 app = express()
 
 app.use(express.static(PATH))
-
+app.get('/',(req,res)=>{
+    res.sendFile('index.html')
+})
 
 app.listen(3000,()=>{
     console.log(`Server live at port: ${port}`)
