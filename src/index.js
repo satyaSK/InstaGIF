eb905f3bc090c0465eb4d236cf93434f10495b20
satyaSK <satyajit.k@somaiya.edu>
2020-03-29 19:34:53 -0700

    init commit

--- src/index.js
+++ src/index.js
@@ -6,9 +6,8 @@ const port = process.env.PORT || 3000
 const PATH = path.join(__dirname,"../public")
 
 app = express()
-//app.use(express.static(PATH))
-process.env.PWD = process.cwd()
-app.use(express.static(path.join(process.env.PWD, 'public')));
+
+app.use(express.static(PATH))
 
 app.listen(3000,()=>{
     console.log(`Server live at port: ${port}`)
