b24d132976cdafc487064c20cfa35632c8709280
satyaSK <satyajit.k@somaiya.edu>
2020-03-30 20:35:46 -0700

    added music feature

--- public/js/client_final.js
+++ public/js/client_final.js
@@ -4,14 +4,23 @@ const trendingBtn = document.querySelector('#trends')
 const instagif = document.querySelector('#joey')
 const body = document.querySelector('#body')
 
+
+
 API_key = 'Upu1PbcVEcikCbr4wFjRcrND4OeSfbIS'
 filters = '&limit=7'
 gifurl = 'https://api.giphy.com/v1/gifs/search?api_key=Upu1PbcVEcikCbr4wFjRcrND4OeSfbIS&q='
 
 
+window.addEventListener('load', (e)=>{
+    //e.preventDefault()
+    loadIntro()
+})
+
 gifInput.addEventListener('keyup',(e)=>{
     //e.preventDefault()
     loadGifQuery()
+    loadmusic()
+    
 })
 
 loadGifQuery = ()=>{
@@ -44,14 +53,40 @@ createHTML = (a)=>{
 
 instagif.addEventListener('click',(e)=>{
     e.preventDefault()
-    random = ["joey","trump","coronavirus","putin","tom cruise","chandler","monica","sacred games","the office","mark zuckerburg","facepalm","dab","bollywood","cute baby"]
-    gifInput.value =  random[Math.floor(Math.random() * random.length)];
+    random = ["joey","trump","justin beiber","macklemore","putin","tom cruise","chandler","monica","sacred games","the office","mark zuckerburg","facepalm","dab","bollywood","cute baby","the social network"]
+    gifInput.value =  random[Math.floor(Math.random() * random.length)]
     loadGifQuery()
+    loadmusic()
 })
 
-// window.addEventListener('load',(e)=>{
-//     e.preventDefault()
-//     random = ["joey","trump","coronavirus","putin","tom cruise","chandler","monica","sacred games","the office","mark zuckerburg","facepalm","dab","bollywood","cute baby"]
-//     gifInput.value =  random[Math.floor(Math.random() * random.length)];
-//     loadGifQuery()
-// })
+loadmusic = ()=>{
+    var xhr = new XMLHttpRequest()
+    var chars = gifInput.value
+    xhr.open('GET','https://api.deezer.com/search?lang=en,hi&limit=3&output=json&q='+chars)//https://cors-anywhere.herokuapp.com/
+    xhr.onload = ()=>{
+        var music = JSON.parse(xhr.responseText)
+        var rand = getrandom(music.data)
+        console.log(music)
+        document.querySelector('#music').src = music.data[rand].preview
+        // document.getElementById("music").play()
+        //document.getElementById('music_muter').muted = false
+        
+    }
+    // xhr.setRequestHeader('Access-Control-Allow-Origin', '*')
+    // xhr.setRequestHeader('Access-Control-Allow-Headers','Content-Type')
+    // xhr.setRequestHeader('Content-Type', 'json/application');
+    xhr.send()
+}
+
+getrandom = (jsonfiledata)=>{
+    return Math.floor(Math.random() * jsonfiledata.length)
+}
+
+loadIntro = ()=>{
+    random = ["dostana","sholay","kabir singh","ariana","dil chahta hai","kal ho na ho","koi mil gaya","dilwale","mission impossible","hrithik roshan "]
+    gifInput.value = random[Math.floor(Math.random() * random.length)];
+    loadmusic()
+    loadGifQuery()
+}
+
+
--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 const express = require("express")
 const path = require('path')
-
+var cors = require('cors')
 
 
 var port = process.env.PORT || 3000
 
 const PATH = path.join(__dirname,"../public")
 app = express()
+app.use(cors()) 
 app.use(express.static(PATH))
 
-app.listen(3000,()=>{
+// app.get('/', (req, res) => {
+//     console.log("heyy")
+//     res.render('index')
+// })
+
+
+app.listen(port,()=>{
     console.log(`Server live at port: ${port}`)
 })
